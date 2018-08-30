64b1db5e71d4cb3c1c92cc0185905a565b968691
Vladislav Pinchuk <deablo@mail.ru>
2018-08-30 13:46:04 +0300

    Add more log functions to bindings

--- algorithms/bindings.js
+++ algorithms/bindings.js
@@ -5,12 +5,13 @@ const tg = (x) => Math.tan(x);
 const tan = tg;
 const ctg = (x) => 1 / Math.tan(x);
 const abs = (x) => Math.abs(x);
-const log = (x) => Math.log(x);
+const ln = (x) => Math.log(x);
+const log2 = (x) => Math.log2(x);
+const log = (x, b) => ln(x)/ln(b);
 const asin = (x) => Math.asin(x);
 const acos = (x) => Math.acos(x);
 const atg = (x) => Math.atan(x);
 const actg = (x) => Math.PI / 2 - Math.atan(x);
-
 
 const pi = Math.PI;
 const e = Math.E;
